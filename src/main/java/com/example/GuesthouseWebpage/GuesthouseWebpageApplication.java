e562bb3af6e2e86921c56ddc6b28d44bbd00b1b4
Ingrid <ingridleichterr@gmail.com>
2022-05-08 13:09:28 +0300

    Creating core for spring application

---
+++ src/main/java/com/example/GuesthouseWebpage/GuesthouseWebpageApplication.java
@@ -0,0 +1,13 @@
+package com.example.GuesthouseWebpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuesthouseWebpageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuesthouseWebpageApplication.class, args);
+	}
+
+}
